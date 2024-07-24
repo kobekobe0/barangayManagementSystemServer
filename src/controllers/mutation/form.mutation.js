2a7378948114b42bb8770dc83d52548d43d72f75
Kobe Brian Santos <kobe0santos0@gmail.com>
2024-07-25 03:06:39 +0800

    updated form creation logic, it now handles all forms in one controller

--- src/controllers/mutation/form.mutation.js
+++ src/controllers/mutation/form.mutation.js
@@ -1,36 +1,42 @@
 import Form from '../../models/Form.js';
+import Business from '../../models/Business.js';
+
+import formDataToRender from '../../helper/formDataToRender.js';
+import generateForm from '../../helper/generateForm.js';
+import getExpirationDate from '../../helper/getExpirationDate.js';
+
+//TODO: in yrsOfResidency, change it to date //only the year and month
+//afected: createForm, getResident
 
 export const createForm = async (req, res) => {
     const form = req.body;
     try{
-        //count number of forms depending on the form type
         const formCount = await Form.countDocuments({ formType: form.formType });
         const paddedFormCount = String(formCount + 1).padStart(6, '0');
 
-        const expirationDate = new Date();
+        const expirationDate = getExpirationDate(form.formType);
 
-        if(form.formType === 'BC'){
-            //add 6 months to the current date
-            expirationDate.setMonth(expirationDate.getMonth() + 6);
-        } else {
-            //dec 31 of the current year
-            expirationDate.setMonth(11);
-            expirationDate.setDate(31);
-        }
-
-        //form the date mm-dd-yyyy
         const formattedExpirationDate = `${expirationDate.getMonth() + 1}-${expirationDate.getDate()}-${expirationDate.getFullYear()}`;
         
-        const newForm = await Form.create(
-            {
-                ...form,
-                formNumber: `${form.formType}-${paddedFormCount}`,
-                expirationDate: formattedExpirationDate
-            }
-        );
-        res.status(201).json({
-            message: "Form created successfully",
-            form: newForm
+        const newForm = await Form.create({
+            ...form,
+            formNumber: `${form.formType}-${paddedFormCount}`,
+            expirationDate: formattedExpirationDate
+        });
+
+        const populatedForm = await Form.findById(newForm._id)
+            .populate('residentID', 'name address picture dateOfBirth placeOfBirth yrsOfResidency')
+            .populate('business')
+
+
+        const dataToRender = await formDataToRender(populatedForm).then(async (data) => {
+            const docxFile = await generateForm(data, form.formType);
+            res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document');
+            res.setHeader('Content-Disposition', `attachment; filename=${form.formType}-${paddedFormCount}.docx`);
+            if(newForm?.business){
+                await Business.findByIdAndUpdate(newForm.business, { $set: { isNew: false } });
+            }    
+            return res.send(docxFile);
         });
     } catch (error) {
         console.log({
@@ -38,7 +44,7 @@ export const createForm = async (req, res) => {
             message: "Failed to create form",
             function: "createForm"
         })
-        res.status(409).json({ 
+        return res.status(409).json({ 
             error: error.message,
             message: "Failed to create form"
         });
